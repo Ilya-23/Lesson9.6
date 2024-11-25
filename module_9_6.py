d343455d6d45f3e257d71a40b6197fabd2b58c8b
Ilya-23 <ilya.gawrin.7635@gmail.com>
2024-11-25 18:09:55 +0700

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,7 +1,15 @@
 def all_variants(text):
-    for i in range(len(text)):
-        for j in range(i, len(text)):
-            yield text[i:j + 1]
+    m = len(text)
+    if m != 0:
+     for i in range(len(text)):
+        m += -1
+        yield text[i]
+     else:
+         for j in range(len(text)):
+             if j < len(text) - 1:
+                 yield text[j : j+2]
+             else:
+                 yield text
 
 a = all_variants("abc")
 for i in a:
